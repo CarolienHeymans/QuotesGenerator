bb6640b1d66e4827e17549f68fe76c38e0527232
Carolien Heymans <heymanscarolien@gmail.com>
2019-06-05 12:43:40 +0200

    quotes

--- program.js
+++ program.js
@@ -1,31 +1,28 @@
-var quotes = [
-	{
-		quote: 'Knufjes!'
-	},
-	{
-		quote: 'Stomkop'
-	},
-	{
-		quote: 'Flapoor'
-	},
-	{
-		quote: 'Als jij doodgaat mag ik je nijntjes, he?'
-	}
-];
 
-// function to load and display a new quote
-function newQuote() {
-	var quoteID = Math.floor(Math.random() * quotes.length);
-	$('#quote').html(quotes[quoteID].quote);
-}
 
-// wait for page load before displaying content
-$(document).ready(function() {
+var quotes = [{
+	"name": "Dries",
+		"quote": 'Knufjes!'
+  }, {
+	"name": "Carolien",
+		"quote": 'Stomkop!'
+  }];
+  
+  // function to load and display a new quote
+  function newQuote() {
+	var quoteID = Math.floor(Math.random() * (quotes.length));
+	$("#quote").html(quotes[quoteID].quote);
+	$("#name").html(quotes[quoteID].name);
+	
+  }
+  
+  // wait for page load before displaying content
+  $(document).ready(function() {
 	// load an initial quote
 	newQuote();
-
+  
 	// retrieve a new quote when the button is clicked
-	$('#generateButton').on('click', function() {
-		newQuote();
+	$("#generateButton").on("click", function() {
+	  newQuote();
 	});
-});
+  });
